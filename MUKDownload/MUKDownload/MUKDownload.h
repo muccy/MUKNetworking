579ac2b2f3e52159c77e82915b554e8a3867c6ec
Muccy <muccymac@gmail.com>
2012-03-19 18:49:00 +0100

    First Commit

---
+++ MUKDownload/MUKDownload/MUKDownload.h
@@ -0,0 +1,13 @@
+//
+//  MUKDownload.h
+//  MUKDownload
+//
+//  Created by Marco Muccinelli on 19/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MUKDownload : NSObject
+
+@end
---
+++ MUKDownload/MUKDownloadTests/MUKDownloadTests.m
@@ -0,0 +1,32 @@
+//
+//  MUKDownloadTests.m
+//  MUKDownloadTests
+//
+//  Created by Marco Muccinelli on 19/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MUKDownloadTests.h"
+
+@implementation MUKDownloadTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MUKDownloadTests");
+}
+
+@end

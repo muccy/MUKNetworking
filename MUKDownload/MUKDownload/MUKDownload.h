2d807779d67ad68d050ca69a36ba685983cfdf86
Muccy <muccymac@gmail.com>
2012-03-19 18:56:48 +0100

    Setup project as static iOS framework (see https://github.com/jverkoey/iOS-Framework)

--- MUKDownload/MUKDownload/MUKDownload.h
+++ MUKDownload/MUKDownload/MUKDownload.h
@@ -1,13 +1,1 @@
-//
-//  MUKDownload.h
-//  MUKDownload
-//
-//  Created by Marco Muccinelli on 19/03/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
-
-@interface MUKDownload : NSObject
-
-@end
